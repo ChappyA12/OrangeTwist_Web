4c411baa3072cc28751fb3cce923c49d6393b434
ChappyA12 <chappy_asel@hotmail.com>
2018-05-24 00:11:20 -0700

    Simplified contact.html slide script

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,211 +1,33 @@
 
+// stores slider values
+let sliders = { "national": 0, "northeast": 0, "other": 0, "sale": 0, "mid": 0};
+
+// sets up slide triggers and hides slide view
+$(document).ready( function() {
+	Object.keys(sliders).forEach( function(key) {
+    $('#contact_slidedown_' + key).slideUp(0);
+		$('#contact_' + key).click( function() { slide(key); });
+	});
+});
+
+// slides a view
+function slide(value) {
+		$('#contact_slidedown_' + value).slideToggle('swing');
+	
+		$('#contact_triangle_' + value).animate (
+			{ borderSpacing: 180 + sliders[value] % 2 * 180 }, 
+			{ step: function(now,fx) { $(this).rotate(now); }, duration:'medium' },
+		'swing');
+		
+		sliders[value] += 1;
+}
+
+// jquery slide shortcut
 jQuery.fn.rotate = function(degrees) {
     $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
-                 '-moz-transform' : 'rotate('+ degrees +'deg)',
-                 '-ms-transform' : 'rotate('+ degrees +'deg)',
-                 'transform' : 'rotate('+ degrees +'deg)'});
+                 '-moz-transform' 	 : 'rotate('+ degrees +'deg)',
+                 '-ms-transform' 		 : 'rotate('+ degrees +'deg)',
+								 '-o-transform' 		 : 'rotate('+ degrees +'deg)',
+                 'transform' 				 : 'rotate('+ degrees +'deg)'});
     return $(this);
 };
-
-var form = 0;
-
-$(document).ready(function(){
-		$('#contact_slidedown_form').slideUp(0);
-});
-
-$(document).ready(function(){
-	$('#contact_form').click(function(){
-		$('#contact_slidedown_form').slideToggle('swing');
-		
-		if (form%2==0) {
-			$('#contact_triangle_form').animate({  borderSpacing: 180 }, {
-    		step: function(now,fx) {
-      		$(this).css('-webkit-transform','rotate('+now+'deg)');
-      		$(this).css('-moz-transform','rotate('+now+'deg)'); 
-      		$(this).css('-ms-transform','rotate('+now+'deg)');
-      		$(this).css('-o-transform','rotate('+now+'deg)');
-      		$(this).css('transform','rotate('+now+'deg)');  
-    		}, duration:'medium' },'swing');
-		}
-		else {
-			$('#contact_triangle_form').animate({  borderSpacing: 360 }, {
-    		step: function(now,fx) {
-      		$(this).css('-webkit-transform','rotate('+now+'deg)');
-      		$(this).css('-moz-transform','rotate('+now+'deg)'); 
-      		$(this).css('-ms-transform','rotate('+now+'deg)');
-      		$(this).css('-o-transform','rotate('+now+'deg)');
-      		$(this).css('transform','rotate('+now+'deg)');  
-    		}, duration:'medium' },'swing');
-		}
-		form ++;
-		
-	});
-});
-
-var national = 0;
-
-$(document).ready(function(){
-		$('#contact_slidedown_national').slideUp(0);
-});
-
-$(document).ready(function(){
-	$('#contact_national').click(function(){
-		$('#contact_slidedown_national').slideToggle('swing');
-		
-		if (national%2==0) {
-			$('#contact_triangle_national').animate({  borderSpacing: 180 }, {
-    		step: function(now,fx) {
-      		$(this).css('-webkit-transform','rotate('+now+'deg)');
-      		$(this).css('-moz-transform','rotate('+now+'deg)'); 
-      		$(this).css('-ms-transform','rotate('+now+'deg)');
-      		$(this).css('-o-transform','rotate('+now+'deg)');
-      		$(this).css('transform','rotate('+now+'deg)');  
-    		}, duration:'medium' },'swing');
-		}
-		else {
-			$('#contact_triangle_national').animate({  borderSpacing: 360 }, {
-    		step: function(now,fx) {
-      		$(this).css('-webkit-transform','rotate('+now+'deg)');
-      		$(this).css('-moz-transform','rotate('+now+'deg)'); 
-      		$(this).css('-ms-transform','rotate('+now+'deg)');
-      		$(this).css('-o-transform','rotate('+now+'deg)');
-      		$(this).css('transform','rotate('+now+'deg)');  
-    		}, duration:'medium' },'swing');
-		}
-		national ++;
-		
-	});
-});
-
-var northeast = 0;
-$(document).ready(function(){
-		$('#contact_slidedown_northeast').slideUp(0);
-});
-$(document).ready(function(){
-	$('#contact_northeast').click(function(){
-		$('#contact_slidedown_northeast').slideToggle('swing');
-		
-		if (northeast%2==0) {
-			$('#contact_triangle_northeast').animate({  borderSpacing: 180 }, {
-    		step: function(now,fx) {
-      		$(this).css('-webkit-transform','rotate('+now+'deg)');
-      		$(this).css('-moz-transform','rotate('+now+'deg)'); 
-      		$(this).css('-ms-transform','rotate('+now+'deg)');
-      		$(this).css('-o-transform','rotate('+now+'deg)');
-      		$(this).css('transform','rotate('+now+'deg)');  
-    		}, duration:'medium' },'swing');
-		}
-		else {
-			$('#contact_triangle_northeast').animate({  borderSpacing: 360 }, {
-    		step: function(now,fx) {
-      		$(this).css('-webkit-transform','rotate('+now+'deg)');
-      		$(this).css('-moz-transform','rotate('+now+'deg)'); 
-      		$(this).css('-ms-transform','rotate('+now+'deg)');
-      		$(this).css('-o-transform','rotate('+now+'deg)');
-      		$(this).css('transform','rotate('+now+'deg)');  
-    		}, duration:'medium' },'swing');
-		}
-		northeast ++;
-		
-	});
-});
-
-var other = 0;
-$(document).ready(function(){
-		$('#contact_slidedown_other').slideUp(0);
-});
-$(document).ready(function(){
-	$('#contact_other').click(function(){
-		$('#contact_slidedown_other').slideToggle('swing');
-		
-		if (other%2==0) {
-			$('#contact_triangle_other').animate({  borderSpacing: 180 }, {
-    		step: function(now,fx) {
-      		$(this).css('-webkit-transform','rotate('+now+'deg)');
-      		$(this).css('-moz-transform','rotate('+now+'deg)'); 
-      		$(this).css('-ms-transform','rotate('+now+'deg)');
-      		$(this).css('-o-transform','rotate('+now+'deg)');
-      		$(this).css('transform','rotate('+now+'deg)');  
-    		}, duration:'medium' },'swing');
-		}
-		else {
-			$('#contact_triangle_other').animate({  borderSpacing: 360 }, {
-    		step: function(now,fx) {
-      		$(this).css('-webkit-transform','rotate('+now+'deg)');
-      		$(this).css('-moz-transform','rotate('+now+'deg)'); 
-      		$(this).css('-ms-transform','rotate('+now+'deg)');
-      		$(this).css('-o-transform','rotate('+now+'deg)');
-      		$(this).css('transform','rotate('+now+'deg)');  
-    		}, duration:'medium' },'swing');
-		}
-		other ++;
-		
-	});
-});
-
-var mid = 0;
-$(document).ready(function(){
-		$('#contact_slidedown_mid').slideUp(0);
-});
-$(document).ready(function(){
-	$('#contact_mid').click(function(){
-		$('#contact_slidedown_mid').slideToggle('swing');
-		
-		if (mid%2==0) {
-			$('#contact_triangle_mid').animate({  borderSpacing: 180 }, {
-    		step: function(now,fx) {
-      		$(this).css('-webkit-transform','rotate('+now+'deg)');
-      		$(this).css('-moz-transform','rotate('+now+'deg)'); 
-      		$(this).css('-ms-transform','rotate('+now+'deg)');
-      		$(this).css('-o-transform','rotate('+now+'deg)');
-      		$(this).css('transform','rotate('+now+'deg)');  
-    		}, duration:'medium' },'swing');
-		}
-		else {
-			$('#contact_triangle_mid').animate({  borderSpacing: 360 }, {
-    		step: function(now,fx) {
-      		$(this).css('-webkit-transform','rotate('+now+'deg)');
-      		$(this).css('-moz-transform','rotate('+now+'deg)'); 
-      		$(this).css('-ms-transform','rotate('+now+'deg)');
-      		$(this).css('-o-transform','rotate('+now+'deg)');
-      		$(this).css('transform','rotate('+now+'deg)');  
-    		}, duration:'medium' },'swing');
-		}
-		mid ++;
-		
-	});
-});
-
-
-var sale = 0;
-$(document).ready(function(){
-		$('#contact_slidedown_sale').slideUp(0);
-});
-$(document).ready(function(){
-	$('#contact_sale').click(function(){
-		$('#contact_slidedown_sale').slideToggle('swing');
-		
-		if (sale%2==0) {
-			$('#contact_triangle_sale').animate({  borderSpacing: 180 }, {
-    		step: function(now,fx) {
-      		$(this).css('-webkit-transform','rotate('+now+'deg)');
-      		$(this).css('-moz-transform','rotate('+now+'deg)'); 
-      		$(this).css('-ms-transform','rotate('+now+'deg)');
-      		$(this).css('-o-transform','rotate('+now+'deg)');
-      		$(this).css('transform','rotate('+now+'deg)');  
-    		}, duration:'medium' },'swing');
-		}
-		else {
-			$('#contact_triangle_sale').animate({  borderSpacing: 360 }, {
-    		step: function(now,fx) {
-      		$(this).css('-webkit-transform','rotate('+now+'deg)');
-      		$(this).css('-moz-transform','rotate('+now+'deg)'); 
-      		$(this).css('-ms-transform','rotate('+now+'deg)');
-      		$(this).css('-o-transform','rotate('+now+'deg)');
-      		$(this).css('transform','rotate('+now+'deg)');  
-    		}, duration:'medium' },'swing');
-		}
-		sale ++;
-		
-	});
-});
--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,11 @@
 
-$(document).ready(function() {
-	$('.standard').hover(
-		function(){
-			$(this).find('.caption').show();
-		},
-		function(){
-			$(this).find('.caption').hide();
-		}
-	);
-	$('.fade').hover(
-		function(){
+$(document).ready( function() {
+	$('.fade').hover (
+		function() {
 			$(this).find('.caption').fadeIn(150);
 		},
-		function(){
-			$(this).find('.caption').fadeOut(020);
-		}
-	);
-	$('.slide').hover(
-		function(){
-			$(this).find('.caption').slideDown(250);
-		},
-		function(){
-			$(this).find('.caption').slideUp(250);
+		function() {
+			$(this).find('.caption').fadeOut(50);
 		}
 	);
 });
